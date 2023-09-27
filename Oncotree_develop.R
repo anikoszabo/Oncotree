b2325e0a4b9e8b8d7b148feb9fda84dc3eb185d1
anikoszabo <aszabo@mcw.edu>
2023-09-27 17:01:31 -0500

    Fix typos and minor issues identified by devtools checks, speed up examples

--- Oncotree_develop.R
+++ Oncotree_develop.R
@@ -6,3 +6,12 @@ ot <- as.package("../Oncotree")
 check(ot, document=FALSE, check_dir = "c:/Temp",
      cran = TRUE, manual=TRUE, incoming=TRUE)
 
+usethis::use_cran_comments()
+
+release_checks()
+spell_check()
+check_win_release()
+
+# does not work with PDF vignettes: https://github.com/r-hub/rhub/issues/556
+# check_rhub()
+
